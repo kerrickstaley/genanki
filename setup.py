087811e9874678c2592fcee669ce374d5909569c
Kerrick Staley <kerrick@kerrickstaley.com>
2017-01-31 00:35:29 -0800

    Add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='genanki',
+      version='0.1',
+      description='Generate Anki decks programmatically',
+      url='http://github.com/kerrickstaley/genanki',
+      author='Kerrick Staley',
+      author_email='k@kerrickstaley.com',
+      license='MIT',
+      packages=['genanki'],
+      zip_safe=False)
