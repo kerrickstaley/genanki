kerrickstaley/genanki

commit 08d8a1394625640f11e928ba725a8ff2fcebcf18

Fix really silly bug getting note_id
TODO: add tests.

genanki/__init__.py
@@ -232,8 +232,9 @@ class Note:
         '',                           # data
     ))
 
+    note_id = cursor.lastrowid
     for card in self.cards:
-      card.write_to_db(cursor, now_ts, deck_id, cursor.lastrowid)
+      card.write_to_db(cursor, now_ts, deck_id, note_id)
 
   def _format_fields(self):
     return '\x1f'.join(self.fields)

commit a4ae33c5863cbe8463309841c95a93bf543f0080

Add test for bug introduced in 08d8a139

tests/test_genanki.py
@@ -99,6 +99,24 @@ class TestWithCollection:
 
     assert imported_deck['name'] == 'foodeck'
 
+  def test_generated_deck_has_valid_cards(self):
+    """
+    Generates a deck with several notes and verifies that the nid/ord combinations on the generated cards make sense.
+
+    Catches a bug that was fixed in 08d8a139.
+    """
+    deck = genanki.Deck(123456, 'foodeck')
+    deck.add_note(genanki.Note(TEST_CN_MODEL, ['a', 'b', 'c']))  # 2 cards
+    deck.add_note(genanki.Note(TEST_CN_MODEL, ['d', 'e', 'f']))  # 2 cards
+    deck.add_note(genanki.Note(TEST_CN_MODEL, ['g', 'h', 'i']))  # 2 cards
+
+    self.import_package(genanki.Package(deck))
+
+    cards = [self.col.getCard(i) for i in self.col.findCards('')]
+
+    # the bug causes us to fail to generate certain cards (e.g. the second card for the second note)
+    assert len(cards) == 6
+
   def test_card_isEmpty__with_2_fields__succeeds(self):
     """Tests for a bug in an early version of genanki where notes with <4 fields were not supported."""
     deck = genanki.Deck(123456, 'foodeck')