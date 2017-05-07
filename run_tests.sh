283d55c0eb7694b5d4de44ffa1e053321513b986
Kerrick Staley <kerrick@kerrickstaley.com>
2017-05-06 22:57:28 -0700

    split out test setup from test run

--- run_tests.sh
+++ run_tests.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 # TODO make this cleaner / platform-independent
 set -e
-anki_test_revision=a98c20a22f3d6e5eb6039bb3e06288dc23c3dec4
 
-install_pyaudio_system_deps() {
-  sudo apt-get install -y python-all-dev portaudio19-dev \
-  || sudo pacman -S --noconfirm portaudio
-}
-
-rm -rf anki_upstream
-git clone https://github.com/dae/anki.git anki_upstream
-( cd anki_upstream
-  git reset --hard $anki_test_revision
-  install_pyaudio_system_deps
-  pip install -r requirements.txt
-)
+# enter venv if needed
+[[ -z "$VIRTUAL_ENV" ]] && source tests_venv/bin/activate
 
 exec python setup.py pytest --addopts tests
