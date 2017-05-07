7c76bf261358ff7194e1aedce71ce780e03083f7
Kerrick Staley <kerrick@kerrickstaley.com>
2017-05-06 22:33:02 -0700

    install_pyaudio_system_deps

--- run_tests.sh
+++ run_tests.sh
@@ -3,19 +3,16 @@
 set -e
 anki_test_revision=a98c20a22f3d6e5eb6039bb3e06288dc23c3dec4
 
-install_pyaudio() {
-  # pyaudio requires other non-Python dependencies, so we install it through
-  # the system package manager before installing Anki's Python dependencies
-  # (it will get skipped by pip)
-  sudo apt-get install -y python3-pyaudio \
-  || sudo pacman -S --noconfirm python-pyaudio
+install_pyaudio_system_deps() {
+  sudo apt-get install -y python-all-dev portaudio19-dev \
+  || sudo pacman -S --noconfirm portaudio
 }
 
 rm -rf anki_upstream
 git clone https://github.com/dae/anki.git anki_upstream
 ( cd anki_upstream
   git reset --hard $anki_test_revision
-  install_pyaudio
+  install_pyaudio_system_deps
   sudo pip3 install -r requirements.txt
 )
 
