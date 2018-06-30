a15ee43b5a72f071746a82969ac786b274343a22
Kerrick Staley <kerrick@kerrickstaley.com>
2018-06-30 10:53:55 -0700

    Install python-virtualenv in setup_tests.sh

--- setup_tests.sh
+++ setup_tests.sh
@@ -5,7 +5,7 @@ anki_test_revision=a98c20a22f3d6e5eb6039bb3e06288dc23c3dec4
 
 # install pyaudio system deps
 sudo apt-get install -y python-all-dev portaudio19-dev \
-|| sudo pacman -S --noconfirm portaudio
+|| sudo pacman -S --noconfirm portaudio python-virtualenv
 
 # enter venv if needed
 if [[ -z "$VIRTUAL_ENV" ]]; then
