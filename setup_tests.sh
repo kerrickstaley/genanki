1682ff04adfb69dcd101b5c92a33e1871593070f
Kerrick Staley <kerrick@kerrickstaley.com>
2018-06-30 11:23:19 -0700

    Fix virtualenv install / usage on Ubuntu

--- setup_tests.sh
+++ setup_tests.sh
@@ -4,12 +4,13 @@ set -e
 anki_test_revision=a98c20a22f3d6e5eb6039bb3e06288dc23c3dec4
 
 # install pyaudio system deps
-sudo apt-get install -y python-all-dev portaudio19-dev \
+sudo apt-get install -y virtualenv portaudio19-dev \
 || sudo pacman -S --noconfirm portaudio python-virtualenv
 
 # enter venv if needed
 if [[ -z "$VIRTUAL_ENV" ]]; then
-  virtualenv3 tests_venv
+  rm -rf tests_venv
+  virtualenv -p python3 tests_venv
   source tests_venv/bin/activate
 fi
 
