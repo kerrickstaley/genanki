b56085109672981b827ee9a14572f8d780aff698
Kerrick Staley <kerrick@kerrickstaley.com>
2018-06-30 12:13:32 -0700

    Fix Travis issues

--- setup_tests.sh
+++ setup_tests.sh
@@ -4,7 +4,7 @@ set -e
 anki_test_revision=a98c20a22f3d6e5eb6039bb3e06288dc23c3dec4
 
 # install pyaudio system deps
-sudo apt-get -y update && sudo apt-get install -y virtualenv portaudio19-dev \
+sudo apt-get -y update && sudo apt-get install -y python-all-dev portaudio19-dev \
 || sudo pacman -S --noconfirm portaudio python-virtualenv
 
 # enter venv if needed
