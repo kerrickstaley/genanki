d9fd1902e3f0ecd59d6495e1154e44cef87a98d9
Jesse Johnson <holocronweaver@gmail.com>
2018-09-16 22:15:54 -0700

    Avoid relative commits in __init__.py

--- genanki/__init__.py
+++ genanki/__init__.py
@@ -11,8 +11,8 @@ import time
 import yaml
 import zipfile
 
-from .apkg_col import APKG_COL
-from .apkg_schema import APKG_SCHEMA
+from genanki.apkg_col import APKG_COL
+from genanki.apkg_schema import APKG_SCHEMA
 
 BASE91_TABLE = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
