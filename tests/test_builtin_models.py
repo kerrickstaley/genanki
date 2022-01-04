e1369a948c112076efcb7f5079ea7816736c8309
Kerrick Staley <kerrick@kerrickstaley.com>
2022-01-03 22:17:56 -0500

    Fix up tests

--- tests/test_builtin_models.py
+++ tests/test_builtin_models.py
@@ -1,6 +1,8 @@
 import genanki
 import os
+import pytest
 import tempfile
+import warnings
 
 
 def test_builtin_models():
@@ -26,11 +28,35 @@ def test_builtin_models():
 
   my_deck.add_note(genanki.Note(
     model=genanki.CLOZE_MODEL,
-    fields=['{{c1::Rome}} is the capital of {{c2::Italy}}']))
+    fields=[
+      '{{c1::Ottawa}} is the capital of {{c2::Canada}}',
+      'Ottawa is in Ontario province.']))
 
-  # Just try writing the note to a .apkg file; if there is no Exception, we assume things are good.
+  # Just try writing the notes to a .apkg file; if there is no Exception and no Warnings, we assume
+  # things are good.
   fnode, fpath = tempfile.mkstemp()
   os.close(fnode)
-  my_deck.write_to_file(fpath)
+
+  with warnings.catch_warnings(record=True) as warning_list:
+    my_deck.write_to_file(fpath)
+
+  assert not warning_list
+
+  os.unlink(fpath)
+
+def test_cloze_with_single_field_warns():
+  my_deck = genanki.Deck(
+    1598559905,
+    'Country Capitals')
+
+  my_deck.add_note(genanki.Note(
+    model=genanki.CLOZE_MODEL,
+    fields=['{{c1::Rome}} is the capital of {{c2::Italy}}']))
+
+  fnode, fpath = tempfile.mkstemp()
+  os.close(fnode)
+
+  with pytest.warns(DeprecationWarning):
+    my_deck.write_to_file(fpath)
 
   os.unlink(fpath)
