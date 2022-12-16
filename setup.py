da9e5a6917e9227b4bb36d9a9b981293ca04f9c6
Gregory Chamberlain <greg@cosine.blue>
2022-12-16 03:32:33 +0000

    Include README.md in package metadata (fix #110) (#113)
    Uses the method described in the Python Packaging User Guide:
    
    https://packaging.python.org/en/latest/guides/making-a-pypi-friendly-readme/#including-your-readme-in-your-package-s-metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+from pathlib import Path
 
 version = {}
 with open('genanki/version.py') as fp:
   exec(fp.read(), version)
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='genanki',
       version=version['__version__'],
       description='Generate Anki decks programmatically',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/kerrickstaley/genanki',
       author='Kerrick Staley',
       author_email='k@kerrickstaley.com',
